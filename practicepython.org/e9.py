81ede3185cfe25ebc349a3102456df737e227291
cc-CATs-LiVE-0000-GYNT-0Ent-ROPy <guest1898786@gmail.com>
2020-05-08 19:38:01 +0200

    guesing while break

---
+++ practicepython.org/e9.py
@@ -0,0 +1,32 @@
+''' https://www.practicepython.org/ 
+Generate a random number between 1 and 9 (including 1 and 9). 
+Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right. 
+(Hint: remember to use the user input lessons from the very first exercise)
+Extras:
+    Keep the game going until the user types “exit”
+    Keep track of how many guesses the user has taken, and when the game ends, print this out.
+'''
+import random
+
+def main():
+  counter=0
+  while True:
+    number = random.randint(1,9)
+    usern = input("Whats your number? ")
+    if usern == "exit":
+      break
+    else:
+      usern = int(usern)
+
+    print(number)
+    counter += 1
+    if number == usern:
+      print("You win after {}".format(counter))
+      counter = 0
+    else:
+      print("You lost")
+
+  
+
+if __name__ == '__main__':
+  main()
