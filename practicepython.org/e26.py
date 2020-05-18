27d5bf0d21447bcb872d99d68b1254803a520c92
cc-CATs-LiVE-0000-GYNT-0Ent-ROPy <guest18.98786@gmail.com>
2020-05-18 22:26:11 +0200

    tictactoe - freez in move(7)

--- practicepython.org/e26.py
+++ practicepython.org/e26.py
@@ -1,44 +1,50 @@
 import numpy as np
 
 def tictactoe(game):
-  win = 0
-  if win < 1:
-    win = diagonal(game)
-  if win < 1:
-    win = line(game)
-  gamerot = np.rot90(game)
-  if win < 1:
-    win = diagonal(gamerot)
-  if win < 1:
-    win = line(gamerot)
-  return win
+  win1 = '0'
+  if win1 == '0':
+    win1 = diagonal(game)
+  if win1 == '0':
+    win1 = line(game)
+    print("returned {} {} {}".format(win1, type(win1), win1 == '0'))
+  if win1 == '0':
+    print("diagonal rot")
+    win1 = diagonal(np.rot90(game))
+  if win1 == '0':
+    win1 = line(np.rot90(game))
+  return win1
 
 def diagonal(game):  
   out = 0
-  res = 0
+  res = '0'
   size = len(game[1])
-  #print(np.array(game))
+  print(np.array(game))
   for i in range(size-1):
-    if game[i][i] == game[i+1][i+1]:
+    if game[i][i] == game[i+1][i+1] and game[i][i] != '0':
       out += 1
+      print("di {} {} {}".format(i, game[i][i],out))
     if out == size - 1:
-      res = game[i][i]
+      res = str(game[i][i])
       break
   return res
 
 def line(game):
   out = 0
-  res = 0
+  res = '0'
   size = len(game[1])
   #print(np.array(game))
+  print(game,"\n")
   for i in range(size):
     for j in range(size -1):
-      if game[i][j] == game[i][j+1]:
+      if game[i][j] == game[i][j+1] and game[i][j] != '0':
         out += 1
+        print("li {} {} {}".format(i, game[i][j],out))
     if out == size - 1:
-      res = game[i][j]
+      res = str(game[i][j])
+      print("res {}".format(res))
       break
     out = 0
+    print("next")
   return res
 
 if __name__ == "__main__":
@@ -46,10 +52,10 @@ if __name__ == "__main__":
            [2, 1, 0],
            [2, 1, 1]]
 
-  game  = [[1, 2, 2, 2],
-           [1, 1, 2, 1],
-           [2, 2, 1, 1],
-           [2, 1, 0, 1]]
+  game  = [[1, 2, 2, 'x'],
+           [1, 1, 2, 'x'],
+           [2, 2, 1, 'x'],
+           [2, 1, 0, 'x']]
 
   print(tictactoe(game))
 
@@ -77,30 +83,30 @@ that space.
 
 Your task this week: given a 3 by 3 list of lists that represents 
 a Tic Tac Toe game board, tell me whether anyone has won, and tell me 
-which player won, if any. A Tic Tac Toe win is 3 in a row - either in 
+which player won, if any. A Tic Tac Toe win1 is 3 in a row - either in 
 a row, a column, or a diagonal. Don’t worry about the case where TWO 
 people have won - assume that in every board there will only be one 
-winner.
+win1ner.
 
 Here are some more examples to work with:
 
-winner_is_2 = [[2, 2, 0],
+win1ner_is_2 = [[2, 2, 0],
   [2, 1, 0],
   [2, 1, 1]]
 
-winner_is_1 = [[1, 2, 0],
+win1ner_is_1 = [[1, 2, 0],
   [2, 1, 0],
   [2, 1, 1]]
 
-winner_is_also_1 = [[0, 1, 0],
+win1ner_is_also_1 = [[0, 1, 0],
   [2, 1, 0],
   [2, 1, 1]]
 
-no_winner = [[1, 2, 0],
+no_win1ner = [[1, 2, 0],
   [2, 1, 0],
   [2, 1, 2]]
 
-also_no_winner = [[1, 2, 0],
+also_no_win1ner = [[1, 2, 0],
   [2, 1, 0],
   [2, 1, 0]]
 
